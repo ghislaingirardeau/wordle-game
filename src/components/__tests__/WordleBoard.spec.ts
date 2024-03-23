2146862d9bd447d17a67c0b1a9e01d62de113646
ghislaingirardeau <g.girardeaupro@sfr.fr>
2024-03-23 10:46:34 +0700

    render victory message

--- src/components/__tests__/WordleBoard.spec.ts
+++ src/components/__tests__/WordleBoard.spec.ts
@@ -1,9 +1,17 @@
-import { mount } from "@vue/test-utils";
+import { DOMWrapper, VueWrapper, mount } from "@vue/test-utils";
 import WordleBoard from "../WordleBoard.vue";
 
 describe("WordleBoard", () => {
-  it("renders properly", () => {
-    const wrapper = mount(WordleBoard, { props: { msg: "Hello Vitest" } });
-    expect(wrapper.text()).toContain("Hello Vitest");
+  it("A victory message when user type the right word", async () => {
+    // Arrange - le mount
+    const wrapper: VueWrapper = mount(WordleBoard, {
+      props: { wordOfTheDay: "TEST" },
+    });
+    // Act - action
+    const guessInput: DOMWrapper<Element> = wrapper.find("input[type=text]");
+    await guessInput.setValue("TEST");
+    await guessInput.trigger("keydown.enter");
+    // Assert - evalue
+    expect(wrapper.text()).toContain("You won!");
   });
 });

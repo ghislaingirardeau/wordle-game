cec1c53473beaa3744516cde386da873aa93307a
ghislaingirardeau <g.girardeaupro@sfr.fr>
2024-03-23 11:18:26 +0700

    extract wrapper to beforeEach

--- README.md
+++ README.md
@@ -2,10 +2,29 @@
 
 ## Installation
 
-Vitest extension: pour lancer les tests et avoir un apercu rapide des resultats
+npm run test: pour voir les resultats des tests en continue
 
 ## Type de test
 
 - Unit test: une ligne de code très précise à tester -> trouvera de suite d'ou vient l'erreur
 - feature test: un composant ou une fonctionnalité (bloc de code) à tester -> scope plus large mais on sera aussi d'ou vient l'erreur
 - End to End: pour controler la user stories de notre app, décrit clairement les étapes de notre utilisateur -> plus long et moins précis
+
+## Principe
+
+Feu rouge:
+
+- Je décris et j'écris le test, ce que j'attends
+- le test renvoie donc ROUGE
+
+Feu vert:
+
+- Je mets à jour mon component ou ma fonction pour que le test passe au vert
+- Une fois résolu le test renvoie VERT
+
+Refactoriser:
+
+- Refactorise le composant ou la fonction
+- Refactorise le fichier test
+
+Enfin je commit
--- src/components/__tests__/WordleBoard.spec.ts
+++ src/components/__tests__/WordleBoard.spec.ts
@@ -6,11 +6,18 @@ import { DEFEAT_MESSAGE, VICTORY_MESSAGE } from "@/settings";
 
 describe("WordleBoard", () => {
   let wordOfTheDay: string = "TEST";
-  test("A victory message when user type the right word", async () => {
+
+  // code qui est commun au test du describe WordleBoard
+  // avant chaque test; execute ce code
+  let wrapper: ReturnType<typeof mount>;
+  // wrapper prendra toujours en retour de la méthode mount()
+  beforeEach((): void => {
     // Arrange - le mount
-    const wrapper: VueWrapper = mount(WordleBoard, {
+    wrapper = mount(WordleBoard, {
       props: { wordOfTheDay: wordOfTheDay },
     });
+  });
+  test("A victory message when user type the right word", async () => {
     // Act - action
     const guessInput: DOMWrapper<Element> = wrapper.find("input[type=text]");
     await guessInput.setValue(wordOfTheDay);
@@ -21,10 +28,6 @@ describe("WordleBoard", () => {
   // placeholder test to do later
   // test.todo("Defeat message appears if the user type the wrong word");
   test("Defeat message appears if the user type the wrong word", async () => {
-    // Arrange - le mount
-    const wrapper: VueWrapper = mount(WordleBoard, {
-      props: { wordOfTheDay: wordOfTheDay },
-    });
     // Act - action
     const guessInput: DOMWrapper<Element> = wrapper.find("input[type=text]");
     await guessInput.setValue("WRONG");
@@ -34,10 +37,6 @@ describe("WordleBoard", () => {
   });
   // si utilisateur n'a rien tapé, je ne renvoie aucun de ces messages
   test("no end of game if the user has not make a guess yet", async () => {
-    // Arrange - le mount
-    const wrapper: VueWrapper = mount(WordleBoard, {
-      props: { wordOfTheDay: wordOfTheDay },
-    });
     // Assert - evalue
     expect(wrapper.text()).not.toContain(DEFEAT_MESSAGE);
     expect(wrapper.text()).not.toContain(VICTORY_MESSAGE);

26706f79d7a76a52e99ecb1e67486f113b7d34fd
ghislaingirardeau <g.girardeaupro@sfr.fr>
2024-03-23 11:02:33 +0700

    variable extraction

--- src/components/__tests__/WordleBoard.spec.ts
+++ src/components/__tests__/WordleBoard.spec.ts
@@ -2,19 +2,35 @@ import { DOMWrapper, VueWrapper, mount } from "@vue/test-utils";
 import WordleBoard from "../WordleBoard.vue";
 //* definir des variables dans settings
 // Utilsable dans le test ET dans le component
-import { VICTORY_MESSAGE } from "@/settings";
+import { DEFEAT_MESSAGE, VICTORY_MESSAGE } from "@/settings";
 
 describe("WordleBoard", () => {
-  it("A victory message when user type the right word", async () => {
+  let wordOfTheDay: string = "TEST";
+  test("A victory message when user type the right word", async () => {
     // Arrange - le mount
     const wrapper: VueWrapper = mount(WordleBoard, {
-      props: { wordOfTheDay: "TEST" },
+      props: { wordOfTheDay: wordOfTheDay },
     });
     // Act - action
     const guessInput: DOMWrapper<Element> = wrapper.find("input[type=text]");
-    await guessInput.setValue("TEST");
+    await guessInput.setValue(wordOfTheDay);
     await guessInput.trigger("keydown.enter");
     // Assert - evalue
     expect(wrapper.text()).toContain(VICTORY_MESSAGE);
   });
+  // placeholder test to do later
+  // test.todo("Defeat message appears if the user type the wrong word");
+  test("Defeat message appears if the user type the wrong word", async () => {
+    // Arrange - le mount
+    const wrapper: VueWrapper = mount(WordleBoard, {
+      props: { wordOfTheDay: wordOfTheDay },
+    });
+    // Act - action
+    const guessInput: DOMWrapper<Element> = wrapper.find("input[type=text]");
+    await guessInput.setValue("WRONG");
+    await guessInput.trigger("keydown.enter");
+    // Assert - evalue
+    expect(wrapper.text()).toContain(DEFEAT_MESSAGE);
+  });
+  test.todo("no end of game if the user has not make a guess");
 });
--- src/settings.ts
+++ src/settings.ts
@@ -1,1 +1,2 @@
 export const VICTORY_MESSAGE: string = "You won!";
+export const DEFEAT_MESSAGE: string = "You lose!";

864f6d95926c308e7468229106dc6354d38e699c
ghislaingirardeau <g.girardeaupro@sfr.fr>
2024-03-26 11:34:27 +0700

    get a random word to guess

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,18 @@
 <script setup lang="ts">
+import { computed } from "vue";
 import WordleBoard from "./components/WordleBoard.vue";
+
+import englishWord from "./utils/englishWordsWith5Letters.json";
+
+const randomIndex = (min: number, max: number): number => {
+  return Math.round(Math.random() * (max - min) + min);
+};
+
+const randomWord = computed(() => {
+  return englishWord[randomIndex(0, 2500)];
+});
 </script>
 
 <template>
-  <wordle-board wordOfTheDay="CARDS" />
+  <wordle-board :wordOfTheDay="randomWord" />
 </template>

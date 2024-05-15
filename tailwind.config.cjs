f05d0560cf31490729e10e890943c4f93311bd7d
ghislaingirardeau <g.girardeaupro@sfr.fr>
2024-05-15 11:49:38 +0800

    css: add tailwind library

---
+++ tailwind.config.cjs
@@ -0,0 +1,23 @@
+module.exports = {
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+    colors: {
+      amber: {
+        light: "#FEF3C7",
+        DEFAULT: "#FCD34D",
+        dark: "#F59E0B",
+      },
+      emerald: {
+        light: "#D1FAE5",
+        DEFAULT: "#6EE7B7",
+        dark: "#10B981",
+      },
+      white: "#60A5FA",
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};

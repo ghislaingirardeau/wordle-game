dff1d82ffc886e17b67e2fe232d0690f6a526e2d
ghislaingirardeau <g.girardeaupro@sfr.fr>
2024-05-20 17:16:57 +0800

    add comment

--- src/settings.ts
+++ src/settings.ts
@@ -3,8 +3,14 @@ import { ref } from "vue";
 export const VICTORY_MESSAGE: string = "You won!";
 export const DEFEAT_MESSAGE: string = "You lose!";
 export const WORD_SIZE: number = 5;
+// PARTAGE DE REF ENTRE COMPONENTS + UTILISABLE DANS LES TESTS !
+// je peux exporter des refs de vue, qui seront utilisable dans les components dés que je l'importe
+// On pourra alors changer sa valeur, mais aussi "watch" les changements dans un autre component
 export const END_GAME_ATTEMPT = ref<number>(10);
+//--------------------------------
 export const LEVELS: string[] = ["Easy", "Medium", "Difficult"];
+
+export const testRefAccessibility = ref<number>(1);
 
 export function SET_DIFFICULTY(type: string) {
   switch (type) {

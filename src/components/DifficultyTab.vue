7d09b0e3323cde2620ecf53e640cd8a1aa3c5a2a
ghislaingirardeau <g.girardeaupro@sfr.fr>
2024-05-21 14:23:40 +0800

    fix: tabs is active on li

--- src/components/DifficultyTab.vue
+++ src/components/DifficultyTab.vue
@@ -8,12 +8,12 @@
         v-for="(level, index) in LEVELS"
         :key="'index-' + index"
         class="w-1/4 rounded hover:text-gray-600 hover:bg-amber"
+        :class="{ active: index == 1 }"
       >
         <a
           href="#"
           :id="'level-' + level.toLowerCase()"
           class="inline-block p-4"
-          :class="{ active: index == 1 }"
           @click="changeDifficulty($event, level)"
           >{{ level }}</a
         >
@@ -30,12 +30,12 @@ const tabs = ref<HTMLElement>();
 
 function changeDifficulty(event: Event, level: string) {
   // remove class active for all
-  tabs.value?.querySelectorAll("a").forEach((element) => {
+  tabs.value?.querySelectorAll("li").forEach((element) => {
     element.classList.remove("active");
   });
   // add active class
   const button = event.target as HTMLElement;
-  button.classList.add("active");
+  button.parentElement?.classList.add("active");
   SET_DIFFICULTY(level);
 }
 </script>

951abeb935f831b7f52946e14db96c64226a086c
ghislaingirardeau <g.girardeaupro@sfr.fr>
2024-06-18 11:54:58 +0800

    feat: display definition button and show definition at the end of the game

--- src/App.vue
+++ src/App.vue
@@ -58,24 +58,6 @@ const wordsListLang = computed(() => {
     );
   }
 });
-
-onMounted(async () => {
-  const url = import.meta.env.VITE_dicolink_url + "cheval/definitions";
-  const options = {
-    method: "GET",
-    headers: {
-      "x-rapidapi-key": import.meta.env.VITE_x_rapidapi_key,
-      "x-rapidapi-host": import.meta.env.VITE_x_rapidapi_host,
-    },
-  };
-  try {
-    const response = await fetch(url, options);
-    const result = await response.json();
-    console.log(result[0].definition);
-  } catch (error) {
-    console.error(error);
-  }
-});
 </script>
 
 <style>
